351614a7e046f1eee78d1d7e89c4164270c0086f
Marija Ćehić <cehicmarija@gmail.com>
2023-06-27 00:18:01 +0200

    Add index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
 	return (
 		<>
 			<main className="relative">
 				<h1 className="bg-blue-200">Index</h1>
+				<Hero />
 			</main>
 		</>
 	);
