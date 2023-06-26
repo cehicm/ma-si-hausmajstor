09368b51b1d2236aee06cb549a3b617f2c7de0c9
Marija Ćehić <cehicmarija@gmail.com>
2023-06-27 00:19:35 +0200

    Add contents to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,6 +17,9 @@ export default function App({ Component, pageProps }) {
 				/>
 			</Head>
 			<Header />
+			<main className={inter.className}>
+				<Component {...pageProps} />
+			</main>
 		</>
 	);
 }
