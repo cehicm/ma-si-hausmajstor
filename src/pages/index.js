4b397c4d9ef3f55b73f9abe09849057dec0c23e0
Marija Ćehić <cehicmarija@gmail.com>
2023-06-28 01:01:43 +0200

    Add Services component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import Services from "@/components/Services";
 
 export default function Home() {
 	return (
 		<>
 			<main className="relative">
 				<Hero />
+				<Services />
 			</main>
 		</>
 	);
