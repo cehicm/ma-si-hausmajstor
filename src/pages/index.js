c740a33bcd60581ea65cced5d4d5a0ebe62042c0
Marija Ćehić <cehicmarija@gmail.com>
2023-07-02 20:37:01 +0200

    Install sharp package

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 
 export default function Home() {
 	return (
@@ -9,6 +10,7 @@ export default function Home() {
 				<Hero />
 				<Services />
 				<About />
+				<Contact />
 			</main>
 		</>
 	);
