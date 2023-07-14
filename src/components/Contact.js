98bff868565ae4a85c9e042b42e9f6f73d9e29d0
Marija Ćehić <cehicmarija@gmail.com>
2023-07-15 00:39:28 +0200

    Update contact component responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
 				<div className="container px-6 md:px-12">
 					<div className="block rounded-lg bg-[hsla(0,0%,100%,0.8)] px-6 py-12 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-[hsla(0,0%,5%,0.7)] dark:shadow-black/20 md:py-16 md:px-12 -mt-[100px] backdrop-blur-[30px]">
 						<div className="flex flex-wrap">
-							<div className="w-full shrink-0 grow-0 basis-auto lg:w-7/12">
+							<div className="w-full shrink-0 grow-0 basis-auto lg:w-3/4 mx-auto">
 								<div className="flex flex-wrap">
 									<div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 md:px-3 lg:w-full lg:px-6 xl:w-6/12">
 										<div className="flex items-start">
