654ed7fd186bb4ac29b06d07c07ae4a8df63c6ff
Marija Ćehić <cehicmarija@gmail.com>
2023-07-25 21:39:12 +0200

    Update CTA to be able to add links

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,13 +1,24 @@
+import Link from "next/link";
 import { motion } from "framer-motion";
 
-export default function CTA() {
-	return (
+export default function CTA({ text, url }) {
+	return url ? (
+		<Link href={url}>
+			<motion.div
+				whileHover={{ scale: [null, 1.5, 1.4] }}
+				transition={{ duration: 0.3 }}
+				className="bg-lightbrown my-8 lg:text-3xl py-8 text-center text-white"
+			>
+				<span>{text}</span>
+			</motion.div>
+		</Link>
+	) : (
 		<div className="bg-lightbrown my-8 lg:text-3xl py-8 text-center text-white">
 			<motion.div
 				whileHover={{ scale: [null, 1.5, 1.4] }}
 				transition={{ duration: 0.3 }}
 			>
-				VAŠA ZAMISAO JE NAŠA MISIJA!
+				{text}
 			</motion.div>
 		</div>
 	);
