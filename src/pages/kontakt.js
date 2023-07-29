5ae7641dc17b5cf044f5317f701a9d9a795a2b1d
Marija Ćehić <cehicmarija@gmail.com>
2023-07-29 18:46:03 +0200

    Add contact page

---
+++ src/pages/kontakt.js
@@ -0,0 +1,25 @@
+// Električarski radovi u Beogradu uključuju opštine:
+
+// Čukarica
+// Novi Beograd
+// Palilula
+// Rakovica
+// Savski venac
+// Stari grad
+// Voždovac
+// Vračar
+// Zemun
+// Zvezdara
+// Barajevo
+// Grocka
+// Lazarevac
+// Mladenovac
+// Obrenovac
+// Sopot
+// Surčin
+
+import React from "react";
+
+export default function kontakt() {
+	return <div>kontakt</div>;
+}
